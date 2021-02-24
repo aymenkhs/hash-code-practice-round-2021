51652165bf5bdd5bfc3dc24825e0f575a3ad30b2
aymmenkhs <aymenkhouas@gmail.com>
2021-02-25 00:58:21 +0100

    init pizza

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import input
 
+class Pizza:
+
+    def __init__(self, pizza_num, nb_ingrediants, list_ingrediants):
+        self.pizza_num = pizza_num
+        self.nb_ingrediants = nb_ingrediants
+        self.list_ingrediants = list_ingrediants
+
+    def __str__(self):
+        return "num:{}\nnb_ingrediants:{}\ningrediants{}\n".format(self.pizza_num,
+            self.nb_ingrediants, self.list_ingrediants)
+
+    def __repr__(self):
+        return str(self)
+
+    def __contains__(self, ingrediant):
+        if ingrediant in self.list_ingrediants:
+            return True
+        else:
+            return False
+
+    def __getitem__(self, index):
+        return self.list_ingrediants[index]
+
+    def __iter__(self):
+        return iter(self.list_ingrediants)
+
+    def __next__(self):
+        return next(self.list_ingrediants)
 
 
 def main():
@@ -12,7 +40,11 @@ def main():
     # separate the folowing lines
     lines = input.separate_content(content)
     # extract the content from each line
-    pizzas = [] 
+    pizzas = []
+    for i in range(nb_pizza):
+        nb_ingredients, ingrediants = input.line_content(lines[i])
+        pizzas.append(Pizza(i, nb_ingredients, ingrediants))
+
 
 if __name__ == '__main__':
     main()
